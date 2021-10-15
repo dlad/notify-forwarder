1f3ab484a957e363416f1dab91736c827c979797
Dipen Lad <dipen.lad@paddle.com>
2021-10-15 08:58:30 +0100

    read in permissions and apply existing permissions

--- src/inject_utimes.cc
+++ src/inject_utimes.cc
@@ -2,6 +2,8 @@
 
 #include <stdexcept>
 #include <stdio.h>
+#include <sys/stat.h>
+#include <string.h>
 
 #include "inject_utimes.h"
 
@@ -10,10 +12,26 @@ UtimesInjectPlugin::~UtimesInjectPlugin() {}
 void UtimesInjectPlugin::inject(const std::vector<std::string>& paths)
 {
     FILE *fp;
+    struct stat buffer;
+    int status;
+
     for (const auto& path : paths) {
-        fp=fopen (path.c_str(), "r");
-	if (fp != NULL) {
-		fclose(fp);
-	}
+        // zero struct to clear out old data
+        memset(&buffer, 0, sizeof(buffer));
+
+        status = stat(path.c_str(), &buffer);
+        if (status != 0) {
+            // stat did not work - maybe file does not exist
+            continue;
+        }
+
+        if (!S_ISREG(buffer.st_mode)) {
+            // file is not a regular file
+            // this will exclude directories
+            continue;
+        }
+
+        // apply the existing permissions as just read in
+        chmod(path.c_str(), buffer.st_mode);
     }
 }
